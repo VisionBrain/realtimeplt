cc897fdc2403b9c0e718347eaef90faf67383a65
Aryan Karn <32276807+Aryan05@users.noreply.github.com>
2020-09-30 21:48:49 +0530

    tf_keras.py (Updated)

--- realtimeplt/inputs/tf_keras.py
+++ realtimeplt/inputs/tf_keras.py
@@ -1,13 +1,13 @@
 import keras
 import tensorflow as tf
 from tensorflow import keras
+from tensorflow.keras import layers
 from .generic_keras import _PlotLossesCallback
 
 
 class PlotLossesCallback(_PlotLossesCallback, keras.callbacks.Callback):
     """Callback for tensorflow keras"""
     def __init__(self, **kwargs):
+        # keras & tensorflow call @ tensorboard
         keras.callbacks.Callback.__init__(self)
         _PlotLossesCallback.__init__(self, **kwargs)
-        
-     return 0   
